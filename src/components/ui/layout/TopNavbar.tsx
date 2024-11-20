aaf07f871c6109b14a549f5e89e8a0548f5104e0
Wisnu Wirayuda <wisnuwirayuda15@gmail.com>
2024-11-20 16:42:18 +0700

    Refactor top navbar

--- src/components/ui/layout/TopNavbar.tsx
+++ src/components/ui/layout/TopNavbar.tsx
@@ -5,13 +5,13 @@ import { usePathname } from "next/navigation";
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link as NextLink, Button } from "@nextui-org/react";
 import BackButton from "@/components/ui/button/BackButton";
 import { useWindowScroll } from "@mantine/hooks";
-// import NavbarMenuItems from "../other/NavbarMenuItems";
 import SearchInput from "../input/SearchInput";
 import ThemeSwitchDropdown from "../input/ThemeSwitchDropdown";
 import clsx from "clsx";
 import FullscreenToggleButton from "../button/FullscreenToggleButton";
 import { Saira } from "@/lib/fonts";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 const TopNavbar = () => {
   const pathName = usePathname();
@@ -20,52 +20,43 @@ const TopNavbar = () => {
   const hrefs = siteConfig.navItems.map((item) => item.href);
   const show = hrefs.includes(pathName);
 
-  return show ? (
+  return (
     <Navbar
       disableScrollHandler
       isBlurred={false}
       position="sticky"
       maxWidth="full"
       classNames={{ wrapper: "px-2 md:px-4" }}
-      className="inset-0 h-min -translate-y-px bg-background"
+      className={cn("inset-0 h-min bg-transparent", {
+        "bg-background": show,
+      })}
     >
+      {!show && <div className="absolute inset-0 h-full w-full border-b border-background bg-background" style={{ opacity: opacity }} />}
       <NavbarBrand>
-        <div>
-          <Link href="/" className={clsx("tracking-widest", Saira.className)} style={{ fontWeight: 600, fontSize: 30 }}>
-            CINEXTMA
-          </Link>
-        </div>
+        {show ? (
+          <div>
+            <Link href="/" className={clsx("tracking-widest", Saira.className)} style={{ fontWeight: 600, fontSize: 30 }}>
+              CINEXTMA
+            </Link>
+          </div>
+        ) : (
+          <BackButton />
+        )}
       </NavbarBrand>
-      <NavbarContent className="hidden w-full max-w-lg gap-2 md:flex" justify="center">
-        <NavbarItem className={clsx("w-full", pathName.startsWith("/search") && "hidden")}>
-          <Link href="/search" className="w-full">
-            <SearchInput className="pointer-events-none" placeholder="Search your favorite movies..." />
-          </Link>
-          {/* <NavbarMenuItems/> */}
-        </NavbarItem>
-      </NavbarContent>
+      {show && (
+        <NavbarContent className="hidden w-full max-w-lg gap-2 md:flex" justify="center">
+          <NavbarItem className={clsx("w-full", pathName.startsWith("/search") && "hidden")}>
+            <Link href="/search" className="w-full">
+              <SearchInput className="pointer-events-none" placeholder="Search your favorite movies..." />
+            </Link>
+          </NavbarItem>
+        </NavbarContent>
+      )}
       <NavbarContent justify="end">
         <NavbarItem className="flex gap-3">
           <ThemeSwitchDropdown />
           <FullscreenToggleButton />
         </NavbarItem>
-      </NavbarContent>
-    </Navbar>
-  ) : (
-    <Navbar
-      disableScrollHandler
-      maxWidth="full"
-      position="sticky"
-      isBlurred={false}
-      className="-translate-y-px bg-transparent"
-      classNames={{ wrapper: "px-2 md:px-4" }}
-    >
-      <div className="absolute inset-0 h-full w-full border-b border-background bg-background" style={{ opacity: opacity }}></div>
-      <NavbarBrand>
-        <BackButton />
-      </NavbarBrand>
-      <NavbarContent justify="end">
-        <FullscreenToggleButton />
       </NavbarContent>
     </Navbar>
   );
