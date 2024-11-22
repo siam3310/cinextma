d600a257a925d5a8eb58e813ae31181a5ad451c9
Wisnu Wirayuda <wisnuwirayuda15@gmail.com>
2024-11-22 18:37:38 +0700

    Refactors IconButton and VaulDrawer components
    Updates IconButton to accept additional props for increased flexibility and removes redundant tooltip placement props.  Adds `onClick` prop alongside existing `onPress`.
    
    Modifies VaulDrawer to simplify trigger handling, adds title support, custom backdrop options, and full-width control.  Improves overall component API and styling.

--- src/components/ui/button/IconButton.tsx
+++ src/components/ui/button/IconButton.tsx
@@ -2,26 +2,14 @@ import { Tooltip, Button, ButtonProps } from "@nextui-org/react";
 import { Icon } from "@iconify/react";
 import React from "react";
 
-interface IconButtonProps extends ButtonProps {
+export interface IconButtonProps extends ButtonProps {
   icon: string | React.ReactNode;
   text?: string;
   tooltip?: string;
   iconSize?: number;
   buttonClassName?: string;
   tooltipClassName?: string;
-  tooltipPlacement?:
-    | "top"
-    | "bottom"
-    | "right"
-    | "left"
-    | "top-start"
-    | "top-end"
-    | "bottom-start"
-    | "bottom-end"
-    | "left-start"
-    | "left-end"
-    | "right-start"
-    | "right-end";
+  tooltipPlacement?: "top" | "bottom" | "right" | "left" | "top-start" | "top-end" | "bottom-start" | "bottom-end" | "left-start" | "left-end" | "right-start" | "right-end";
 }
 
 export const IconButton: React.FC<IconButtonProps> = ({
@@ -34,23 +22,16 @@ export const IconButton: React.FC<IconButtonProps> = ({
   tooltipClassName,
   tooltipPlacement,
   onPress,
+  onClick,
   radius,
   size,
   color,
   variant,
+  as,
 }) => {
   return (
     <Tooltip isDisabled={!tooltip} content={tooltip} placement={tooltipPlacement} className={tooltipClassName}>
-      <Button
-        isDisabled={isDisabled}
-        isIconOnly={!text}
-        radius={radius}
-        onPress={onPress}
-        size={size}
-        color={color}
-        variant={variant}
-        className={buttonClassName}
-      >
+      <Button as={as} isDisabled={isDisabled} isIconOnly={!text} radius={radius} onPress={onPress} onClick={onClick} size={size} color={color} variant={variant} className={buttonClassName}>
         {typeof icon === "string" ? <Icon icon={icon} fontSize={iconSize ?? 24} /> : icon} {text}
       </Button>
     </Tooltip>
--- src/components/ui/overlay/VaulDrawer.tsx
+++ src/components/ui/overlay/VaulDrawer.tsx
@@ -1,40 +1,41 @@
 "use client";
 
-import { cn } from '@/lib/utils';
-import { Button } from '@nextui-org/button';
-import { Drawer } from 'vaul';
-
+import { cn } from "@/lib/utils";
+import { Button } from "@nextui-org/button";
+import { Drawer } from "vaul";
 
 interface DrawerProps {
-  children?: React.ReactNode;
-  trigger?: React.ReactNode;
-  direction?: "right" | "top" | "bottom" | "left" | undefined;
-  isOverlayBlurred?: boolean;
+  children: React.ReactNode;
+  trigger: React.ReactNode;
+  title?: React.ReactNode;
+  direction?: "right" | "top" | "bottom" | "left";
+  backdrop?: "opaque" | "blur" | "transparent";
+  fullWidth?: boolean;
 }
 
-const VaulDrawer: React.FC<DrawerProps> = ({ children, trigger, direction, isOverlayBlurred }) => {
+export default function VaulDrawer({ children, trigger, title, direction, backdrop = "opaque", fullWidth }: DrawerProps) {
   return (
-    <Drawer.Root direction={direction} shouldScaleBackground >
-      <Drawer.Trigger asChild>
-        <Button>
-          {trigger}
-        </Button>
-      </Drawer.Trigger>
+    <Drawer.Root direction={direction}>
+      <Drawer.Trigger asChild>{typeof trigger === "string" ? <Button>{trigger}</Button> : trigger}</Drawer.Trigger>
       <Drawer.Portal>
-        <Drawer.Overlay className={cn("fixed inset-0 bg-black/70 z-[9998]", {
-          "backdrop-blur-sm": isOverlayBlurred
-        })} />
-        <Drawer.Content className="text-foreground bg-secondary-background flex flex-col rounded-t-2xl mt-24 h-fit w-full max-h-[97%] fixed bottom-0 left-0 right-0 outline-none z-[9999] place-self-center">
-          <div className="pt-4 pb-6 px-6 rounded-t-2xl flex-1">
-            <div aria-hidden className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-foreground/50 mb-5" />
-            <div className="max-w-xl mx-auto">
-              {children}
-            </div>
+        <Drawer.Overlay
+          className={cn("fixed inset-0 z-[9998] bg-black/70", {
+            "backdrop-blur-sm": backdrop === "blur",
+            "bg-transparent": backdrop === "transparent",
+          })}
+        />
+        <Drawer.Content
+          className={cn("fixed bottom-0 left-0 right-0 z-[9999] mt-24 flex h-fit max-h-[97%] w-full flex-col place-self-center rounded-t-2xl bg-secondary-background text-foreground outline-none", {
+            "md:w-max": !fullWidth,
+          })}
+        >
+          <div className="flex-1 space-y-5 rounded-t-2xl pb-6 pt-4">
+            <div aria-hidden className="mx-auto h-1.5 w-12 flex-shrink-0 rounded-full bg-foreground/50" />
+            <Drawer.Title className="text-center text-xl">{title}</Drawer.Title>
+            <div className="mx-auto max-w-lg">{children}</div>
           </div>
         </Drawer.Content>
       </Drawer.Portal>
     </Drawer.Root>
   );
 }
-
-export default VaulDrawer
