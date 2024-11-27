c0ae47554b082b064833836162637adc24ba66bc
Wisnu Wirayuda <wisnuwirayuda15@gmail.com>
2024-11-27 14:56:11 +0700

    Adds fullscreen toggle conditional for PWA installs
    Improves fullscreen toggle behavior for Progressive Web Apps (PWAs).
    
    The toggle button now only appears if the app isn't running in a PWA mode.  This prevents the toggle from appearing when it is unnecessary and likely confusing to users in such a context.

--- src/components/ui/button/FullscreenToggleButton.tsx
+++ src/components/ui/button/FullscreenToggleButton.tsx
@@ -1,11 +1,12 @@
-import { useFullscreen } from "@mantine/hooks";
+import { useFullscreen, useMediaQuery } from "@mantine/hooks";
 import IconButton from "./IconButton";
 import { MdFullscreenExit, MdFullscreen } from "react-icons/md";
 
 export default function FullscreenToggleButton() {
   const { toggle, fullscreen } = useFullscreen();
+  const pwaInstalled = useMediaQuery("(display-mode: standalone)");
   const icon = fullscreen ? <MdFullscreenExit className="size-full" /> : <MdFullscreen className="size-full" />;
   const tooltip = fullscreen ? "Minimize" : "Fullscreen";
 
-  return <IconButton tooltip={tooltip} tooltipPlacement="left" className="p-2" icon={icon} onPress={toggle} variant="light" />;
+  if (!pwaInstalled) return <IconButton tooltip={tooltip} tooltipPlacement="left" className="p-2" icon={icon} onPress={toggle} variant="light" />;
 }
