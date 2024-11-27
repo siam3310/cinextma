1df3bed194d265c5fc9fb700de16824c386dab9b
Wisnu Wirayuda <wisnuwirayuda15@gmail.com>
2024-11-27 15:30:47 +0700

    Replaces hardcoded movie player sources with dynamic retrieval
    Improves movie player selection by fetching sources dynamically using a new function.
    Removes hardcoded player URLs for better maintainability and extensibility.

--- src/app/movie/[id]/MoviePlayer.tsx
+++ src/app/movie/[id]/MoviePlayer.tsx
@@ -1,4 +1,5 @@
-import { IconButton } from "@/components/ui/button/IconButton";
+import IconButton from "@/components/ui/button/IconButton";
+import { getMoviePlayers } from "@/lib/players";
 import { getImageUrl, mutateMovieTitle } from "@/lib/utils";
 import { Card, Tabs, Tab, Image, Skeleton } from "@nextui-org/react";
 import { useState } from "react";
@@ -9,39 +10,17 @@ const MoviePlayer: React.FC<{ movie: MovieDetails }> = ({ movie }) => {
   const [playMovie, setPlayMovie] = useState(false);
   const backdropImage = getImageUrl(movie.backdrop_path, "backdrop", true);
   const title = mutateMovieTitle(movie);
-  const id = movie.id;
-  const players = [
-    {
-      title: "VidSrc 1",
-      source: `https://vidsrc.xyz/embed/movie/${id}`,
-    },
-    {
-      title: "VidSrc 2",
-      source: `https://vidsrc.to/embed/movie/${id}`,
-    },
-    {
-      title: "SuperEmbed",
-      source: `https://multiembed.mov/?video_id=${id}&tmdb=1`,
-    },
-    {
-      title: "NontonGo",
-      source: `https://www.nontongo.win/embed/movie/${id}`,
-    },
-    {
-      title: "MoviesAPI",
-      source: `https://moviesapi.club/movie/${id}`,
-    },
-  ];
+  const players = getMoviePlayers(movie.id);
 
   return (
     <section id="movie-player" className="aspect-video size-auto">
       {playMovie ? (
         <Tabs size="md" radius="full" aria-label="Movie Player" placement="bottom" color="primary" className="z-[3]" classNames={{ base: "flex justify-center" }}>
-          {players.map((player) => (
-            <Tab key={player.title} title={player.title}>
+          {players.map(({ title, source }) => (
+            <Tab key={title} title={title}>
               <Card shadow="md" className="relative">
                 <Skeleton className="absolute aspect-video size-full" />
-                <iframe className="z-10 aspect-video size-full" src={player.source} allowFullScreen />
+                <iframe className="z-10 aspect-video size-full" src={source} allowFullScreen />
               </Card>
             </Tab>
           ))}
@@ -49,16 +28,7 @@ const MoviePlayer: React.FC<{ movie: MovieDetails }> = ({ movie }) => {
       ) : (
         <Card shadow="md" className="group aspect-video size-full">
           <Image isBlurred alt={title} className="size-full" classNames={{ wrapper: "absolute-center aspect-video size-full group-hover:opacity-70 transition" }} src={backdropImage} />
-          <IconButton
-            icon={<FaPlay />}
-            radius="full"
-            tooltip={`Play ${title}`}
-            buttonClassName="absolute-center"
-            color="warning"
-            variant="faded"
-            size="lg"
-            onPress={() => setPlayMovie(true)}
-          ></IconButton>
+          <IconButton icon={<FaPlay />} radius="full" tooltip={`Play ${title}`} className="absolute-center" color="warning" variant="faded" size="lg" onPress={() => setPlayMovie(true)}></IconButton>
         </Card>
       )}
     </section>
