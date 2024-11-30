1f9cf54125176e5f4d8f87d863752fe9d1980faf
Wisnu Wirayuda <wisnuwirayuda15@gmail.com>
2024-11-30 19:53:23 +0700

    Removes comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,6 @@ export default function Home() {
       {siteConfig.queryLists.movies.map((movie) => (
         <HomeMovieList key={movie.name} name={movie.name} query={movie.query} param={movie.param} />
       ))}
-
-      {/* <div className="h-24 w-72 relative">
-        <div className="absolute h-full w-full bg-red-900 inset-0 z-0"></div>
-        <div className="absolute h-full w-full bg-gradient-to-r from-foreground inset-0 z-10"></div>
-        <div className="absolute h-full w-full bg-gradient-to-l from-foreground inset-0 z-10"></div>
-      </div> */}
     </div>
   );
 }
