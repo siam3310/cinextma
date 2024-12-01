d9a86b3c002bc40facae97ff7d03680dea2216df
Wisnu Wirayuda <wisnuwirayuda15@gmail.com>
2024-12-01 15:36:49 +0700

    Conditionally renders related movie tabs
    Improves the related movies section by only displaying tabs if related movies are available.  This prevents showing empty tabs and improves the user experience.

--- src/app/movie/[id]/RelatedSection.tsx
+++ src/app/movie/[id]/RelatedSection.tsx
@@ -5,15 +5,22 @@ import { Tab, Tabs } from "@nextui-org/tabs";
 import RelatedMovieList from "./RelatedMovieList";
 
 const RelatedSection: React.FC<{ movie: AppendToResponse<MovieDetails, ("recommendations" | "similar")[], "movie"> }> = ({ movie }) => {
+  const recommendations = movie.recommendations.results as Movie[];
+  const similar = movie.similar.results as Movie[];
+
   return (
     <section id="related" className="z-[3]">
       <Tabs aria-label="Related Section" variant="underlined">
-        <Tab key="recommendations" title="Recommendations">
-          <RelatedMovieList movies={movie?.recommendations.results as Movie[]} />
-        </Tab>
-        <Tab key="similar" title="Similar">
-          <RelatedMovieList movies={movie?.similar.results as Movie[]} />
-        </Tab>
+        {recommendations.length > 0 && (
+          <Tab key="recommendations" title="Recommendations">
+            <RelatedMovieList movies={recommendations} />
+          </Tab>
+        )}
+        {similar.length > 0 && (
+          <Tab key="similar" title="Similar">
+            <RelatedMovieList movies={similar} />
+          </Tab>
+        )}
       </Tabs>
     </section>
   );
