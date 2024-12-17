7a644373bc79b8d602a981503318918dd8169d01
Wisnu Wirayuda <wisnuwirayuda15@gmail.com>
2024-12-17 10:34:22 +0700

    Removes Fullscreen plugin from Gallery
    Updates Gallery component to exclude Fullscreen plugin from lightbox
    Simplifies plugin imports for better readability

--- src/components/ui/overlay/Gallery.tsx
+++ src/components/ui/overlay/Gallery.tsx
@@ -1,12 +1,5 @@
 import Lightbox, { LightboxExternalProps } from "yet-another-react-lightbox";
-import {
-  Captions,
-  Thumbnails,
-  Counter,
-  Zoom,
-  Download,
-  Fullscreen,
-} from "yet-another-react-lightbox/plugins";
+import { Captions, Thumbnails, Counter, Zoom, Download } from "yet-another-react-lightbox/plugins";
 import "yet-another-react-lightbox/styles.css";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
 import "yet-another-react-lightbox/plugins/counter.css";
@@ -15,7 +8,7 @@ import "yet-another-react-lightbox/plugins/captions.css";
 const Gallery: React.FC<Omit<LightboxExternalProps, "plugins">> = ({ ...props }) => {
   return (
     <Lightbox
-      plugins={[Captions, Thumbnails, Counter, Zoom, Download, Fullscreen]}
+      plugins={[Captions, Thumbnails, Counter, Zoom, Download]}
       thumbnails={{ showToggle: true, borderStyle: "none", imageFit: "cover" }}
       captions={{ showToggle: true, descriptionTextAlign: "center", hidden: true }}
       {...props}
