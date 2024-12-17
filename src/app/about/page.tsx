c5f9cb2c74c4d067c0dc8194fc21aa553317fb47
Wisnu Wirayuda <wisnuwirayuda15@gmail.com>
2024-12-17 15:21:29 +0700

    Enhances about page with metadata and GitHub link
    Adds metadata for better SEO
    Includes a GitHub link for social interaction

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,22 @@
 import FAQ from "./FAQ";
+import { FaGithub } from "react-icons/fa6";
+import { siteConfig } from "@/config/site";
+import Link from "next/link";
+import { Metadata } from "next/dist/lib/metadata/types/metadata-interface";
+
+export const metadata: Metadata = {
+  title: `About | ${siteConfig.name}`,
+};
 
 export default function AboutPage() {
   return (
     <>
       <div className="flex w-full justify-center">
-        <div className="w-full max-w-2xl">
+        <div className="flex w-full max-w-2xl flex-col gap-5">
           <FAQ />
+          <Link target="_blank" href={siteConfig.socials.github} className="flex justify-center">
+            <FaGithub size={30} />
+          </Link>
         </div>
       </div>
     </>
