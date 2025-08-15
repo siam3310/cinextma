7fe3f4bb9e82a8f75cc3afc97def57507ab15701
Wisnu Wirayuda <wisnuwirayuda15@gmail.com>
2025-08-15 19:11:15 +0700

    Ensures TMDB access token is defined
    Adds a check to ensure the TMDB access token is defined before initializing the TMDB client.
    
    This prevents the application from crashing or behaving unexpectedly if the environment variable is not set.

--- src/api/tmdb.ts
+++ src/api/tmdb.ts
@@ -1,5 +1,10 @@
+import { isEmpty } from "@/utils/helpers";
 import { TMDB } from "tmdb-ts";
 
-const token = process.env.NEXT_PUBLIC_TMDB_ACCESS_TOKEN ?? "";
+const token = process.env.NEXT_PUBLIC_TMDB_ACCESS_TOKEN!;
+
+if (isEmpty(token)) {
+  throw new Error("TMDB_ACCESS_TOKEN is not defined");
+}
 
 export const tmdb = new TMDB(token);
