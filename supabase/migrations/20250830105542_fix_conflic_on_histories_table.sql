dcafac46495a38f12ccf56a6a52c0f9d017ae896
Wisnu Wirayuda <wisnuwirayuda15@gmail.com>
2025-09-02 23:44:18 +0700

    Squashes schema migrations

--- supabase/migrations/20250830105542_fix_conflic_on_histories_table.sql
+++
@@ -1,9 +0,0 @@
-drop index if exists "public"."unique_movie_history";
-
-drop index if exists "public"."unique_tv_history";
-
-CREATE UNIQUE INDEX histories_user_id_media_id_media_type_season_episode_key ON public.histories USING btree (user_id, media_id, media_type, season, episode);
-
-alter table "public"."histories" add constraint "histories_user_id_media_id_media_type_season_episode_key" UNIQUE using index "histories_user_id_media_id_media_type_season_episode_key";
-
-
