00d8b9b95ca52d7c84901611aaf86b5746c668ff
mahamudunn1005 <mahamudunn1005@gmail.com>
2025-11-25 18:41:18 +0000

    deleting supabase

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,7 @@
 import { type NextRequest } from "next/server";
-import { updateSession } from "@/utils/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
-  // update user's auth session
-  return await updateSession(request);
+  // middleware logic here
 }
 
 export const config = {
--- src/utils/env.ts
+++ src/utils/env.ts
@@ -4,20 +4,14 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     PROTECTED_PATHS: z.string().optional(),
-    SUPABASE_SERVICE_ROLE_KEY: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_TMDB_ACCESS_TOKEN: z.string().min(1),
-    NEXT_PUBLIC_SUPABASE_URL: z.url().min(1),
-    NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY: z.string().min(1),
     NEXT_PUBLIC_AVATAR_PROVIDER_URL: z.url().optional(),
   },
   runtimeEnv: {
     PROTECTED_PATHS: process.env.PROTECTED_PATHS,
-    SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
     NEXT_PUBLIC_TMDB_ACCESS_TOKEN: process.env.NEXT_PUBLIC_TMDB_ACCESS_TOKEN,
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY,
     NEXT_PUBLIC_AVATAR_PROVIDER_URL: process.env.NEXT_PUBLIC_AVATAR_PROVIDER_URL,
   },
 });
