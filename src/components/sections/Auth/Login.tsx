e80c1f34e65bd1f855a291914e2b18942d57cc51
mahamudunn1005 <mahamudunn1005@gmail.com>
2025-11-25 07:11:45 +0000

    captcha-free!!

--- src/components/sections/Auth/Login.tsx
+++ src/components/sections/Auth/Login.tsx
@@ -1,25 +1,19 @@
 import { signIn } from "@/actions/auth";
 import PasswordInput from "@/components/ui/input/PasswordInput";
 import { LoginFormSchema } from "@/schemas/auth";
-import { isEmpty } from "@/utils/helpers";
 import { Google, LockPassword, Mail } from "@/utils/icons";
-import { addToast, Button, Divider, Input, Link } from "@heroui/react";
+import { addToast, Button, Divider, Input, Link } from "heroui/react";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Turnstile } from "@marsidev/react-turnstile";
-import { useCallback, useState } from "react";
 import { useForm } from "react-hook-form";
 import { AuthFormProps } from "./Forms";
-import { env } from "@/utils/env";
 import { useRouter } from "@bprogress/next/app";
 import GoogleLoginButton from "@/components/ui/button/GoogleLoginButton";
 
 const AuthLoginForm: React.FC<AuthFormProps> = ({ setForm }) => {
   const router = useRouter();
-  const [isVerifying, setIsVerifying] = useState(false);
 
   const {
     register,
-    setValue,
     handleSubmit,
     formState: { errors, isSubmitting },
   } = useForm({
@@ -32,11 +26,6 @@ const AuthLoginForm: React.FC<AuthFormProps> = ({ setForm }) => {
   });
 
   const onSubmit = handleSubmit(async (data) => {
-    if (isEmpty(data.captchaToken)) {
-      setIsVerifying(true);
-      return;
-    }
-
     const { success, message } = await signIn(data);
 
     addToast({
@@ -44,29 +33,10 @@ const AuthLoginForm: React.FC<AuthFormProps> = ({ setForm }) => {
       color: success ? "success" : "danger",
     });
 
-    if (!success) {
-      setValue("captchaToken", undefined);
-      setIsVerifying(false);
-      return;
+    if (success) {
+      return router.push("/");
     }
-
-    return router.push("/");
   });
-
-  const onCaptchaSuccess = useCallback(
-    (token: string) => {
-      setValue("captchaToken", token);
-      setIsVerifying(false);
-      onSubmit();
-    },
-    [setValue, setIsVerifying, onSubmit],
-  );
-
-  const getButtonText = useCallback(() => {
-    if (isSubmitting) return "Signing In...";
-    if (isVerifying) return "Verifying...";
-    return "Sign In";
-  }, [isSubmitting, isVerifying]);
 
   return (
     <div className="flex flex-col gap-5">
@@ -84,7 +54,7 @@ const AuthLoginForm: React.FC<AuthFormProps> = ({ setForm }) => {
           type="email"
           variant="underlined"
           startContent={<Mail className="text-xl" />}
-          isDisabled={isSubmitting || isVerifying}
+          isDisabled={isSubmitting}
         />
         <PasswordInput
           {...register("loginPassword")}
@@ -95,33 +65,26 @@ const AuthLoginForm: React.FC<AuthFormProps> = ({ setForm }) => {
           label="Password"
           placeholder="Enter your password"
           startContent={<LockPassword className="text-xl" />}
-          isDisabled={isSubmitting || isVerifying}
+          isDisabled={isSubmitting}
         />
         <div className="flex w-full items-center justify-end px-1 py-2">
           <Link
             size="sm"
             className="text-foreground cursor-pointer"
             onClick={() => setForm("forgot")}
-            isDisabled={isSubmitting || isVerifying}
+            isDisabled={isSubmitting}
           >
             Forgot password?
           </Link>
         </div>
-        {isVerifying && (
-          <Turnstile
-            className="flex h-fit w-full items-center justify-center"
-            siteKey={env.NEXT_PUBLIC_CAPTCHA_SITE_KEY}
-            onSuccess={onCaptchaSuccess}
-          />
-        )}
         <Button
           className="mt-4"
           color="primary"
           type="submit"
           variant="shadow"
-          isLoading={isSubmitting || isVerifying}
+          isLoading={isSubmitting}
         >
-          {getButtonText()}
+          {isSubmitting ? "Signing In..." : "Sign In"}
         </Button>
       </form>
       <div className="flex items-center gap-4">
@@ -129,7 +92,7 @@ const AuthLoginForm: React.FC<AuthFormProps> = ({ setForm }) => {
         <p className="text-tiny text-default-500 shrink-0">OR</p>
         <Divider className="flex-1" />
       </div>
-      <GoogleLoginButton isDisabled={isSubmitting || isVerifying} />
+      <GoogleLoginButton isDisabled={isSubmitting} />
       <p className="text-small text-center">
         Don't have an account?
         <Link
@@ -137,7 +100,7 @@ const AuthLoginForm: React.FC<AuthFormProps> = ({ setForm }) => {
           size="sm"
           className="cursor-pointer"
           onClick={() => setForm("register")}
-          isDisabled={isSubmitting || isVerifying}
+          isDisabled={isSubmitting}
         >
           Sign Up
         </Link>
--- src/utils/env.ts
+++ src/utils/env.ts
@@ -10,7 +10,6 @@ export const env = createEnv({
     NEXT_PUBLIC_TMDB_ACCESS_TOKEN: z.string().min(1),
     NEXT_PUBLIC_SUPABASE_URL: z.url().min(1),
     NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY: z.string().min(1),
-    NEXT_PUBLIC_CAPTCHA_SITE_KEY: z.string().optional(),
     NEXT_PUBLIC_AVATAR_PROVIDER_URL: z.url().optional(),
   },
   experimental__runtimeEnv: {
@@ -19,7 +18,6 @@ export const env = createEnv({
     NEXT_PUBLIC_TMDB_ACCESS_TOKEN: process.env.NEXT_PUBLIC_TMDB_ACCESS_TOKEN,
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY,
-    NEXT_PUBLIC_CAPTCHA_SITE_KEY: process.env.NEXT_PUBLIC_CAPTCHA_SITE_KEY,
     NEXT_PUBLIC_AVATAR_PROVIDER_URL: process.env.NEXT_PUBLIC_AVATAR_PROVIDER_URL,
   },
 });
